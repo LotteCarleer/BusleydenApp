4bc64c0ca436050df5a4ea54fdb4cfd3f0459770
LotteCarleer <r1042412@student.thomasmore.be>
2026-05-31 16:03:14 +0200

    Lotte-eindopdracht-added header to app

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View } from "react-native";
 import ProductCard from "./components/ProductCard";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Ionicons } from "@expo/vector-icons";
 
 import HomeScreen from "./screens/HomeScreen";
 import ProductDetail from "./screens/ProductDetail";
@@ -14,13 +15,66 @@ const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Details" component={ProductDetail} />
-        <Stack.Screen name="BlogDetail" component={BlogDetail} />
-        <Stack.Screen name="CampusDetail" component={CampusDetail} />
-        <Stack.Screen name="SchoolGame" component={SchoolGameScreen} />
+        <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: "#7aaa25",
+          },
+          headerTintColor: "#fff",
+          headerTitleStyle: {
+            fontWeight: "bold",
+            fontSize: 22,
+          },
+          headerBackTitleVisible: false,
+        }}
+      >
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            title: "Busleyden App",
+            headerRight: () => (
+              <Ionicons
+                name="school"
+                size={26}
+                color="white"
+              />
+            ),
+          }}
+        />
+
+        <Stack.Screen
+          name="Details"
+          component={ProductDetail}
+          options={{
+            title: "Product",
+          }}
+        />
+
+        <Stack.Screen
+          name="BlogDetail"
+          component={BlogDetail}
+          options={{
+            title: "Blog",
+          }}
+        />
+
+        <Stack.Screen
+          name="CampusDetail"
+          component={CampusDetail}
+          options={{
+            title: "Campus",
+          }}
+        />
+
+        <Stack.Screen
+          name="SchoolGame"
+          component={SchoolGameScreen}
+          options={{
+            title: "📚 School Game",
+          }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
