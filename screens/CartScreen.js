f77b6a4c617341c84b3eb34d100bbde5f90d4653
LotteCarleer <r1042412@student.thomasmore.be>
2026-06-01 19:53:44 +0200

    Lotte-eindopdracht-New screen for shopping cart

---
+++ screens/CartScreen.js
@@ -0,0 +1,24 @@
+import { View, Text, StyleSheet } from "react-native";
+
+export default function CartScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>🛒 Winkelmandje</Text>
+      <Text>Je winkelmandje is nog leeg.</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  title: {
+    fontSize: 28,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+});
